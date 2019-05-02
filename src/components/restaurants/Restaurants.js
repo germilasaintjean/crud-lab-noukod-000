55140e633712fbbfbcf1239bd3734581f56b4229
Saint-Jean Germila <germila@noukod.com>
2019-05-02 18:21:43 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -10,4 +10,24 @@ class Restaurants extends Component {
   }
 };
 
-export default Restaurants;
+export default Restaurants    
+import React from 'react'
+import Restaurant from './Restaurant'
+
+const Restaurants = props => {
+
+	const restaurants = props.restaurants.map(restaurant => 
+			<Restaurant key={restaurant.id} 
+			 restaurant={restaurant} 
+			 deleteRestaurant={props.deleteRestaurant} 
+			/>
+	)
+  
+    return(
+      <ul>
+        {restaurants}
+      </ul>
+    )
+}
+
+export default Restaurants
