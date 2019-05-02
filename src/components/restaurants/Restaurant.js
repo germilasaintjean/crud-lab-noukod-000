c7e1d60690b04ab85b5691087ff0bfff6b1cd844
Saint-Jean Germila <germila@noukod.com>
2019-05-02 18:21:23 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,40 +1,29 @@
+
 import React, { Component } from 'react'
+import ReviewsContainer from '../../containers/ReviewsContainer'
 
-class RestaurantInput extends Component {
+class Restaurant extends Component {
 
-  state = {
-    text: ''
-  }
-
-  handleOnChange = event => {
-    this.setState({
-      text: event.target.value
-    })
-  }
-
-  handleOnSubmit = event => {
-    event.preventDefault();
-    this.props.addRestaurant(this.state.text)
-    this.setState({
-      text: ''
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleOnSubmit} >
-          <label>Add restaurant: </label>
-          <input
-            type="text"
-            value={this.state.text}
-            onChange={this.handleOnChange} />
-          <label> </label>
-          <input type="submit" />
-        </form>
-      </div>
-    )
-  }
+handleOnClick = () => {
+this.props.deleteRestaurant(this.props.restaurant.id)
 }
 
-export default RestaurantInput
+render() {
+const {restaurant} = this.props
+
+return (
+  this.props.restaurant.id !== '' ?
+  <div>
+    <br />
+    <li>
+      {restaurant.text}
+      <label> </label><button onClick={this.handleOnClick}>X</button>
+      <ReviewsContainer restaurant={restaurant}/>
+    </li>
+  </div> :
+  null
+)
+}
+}
+
+export default Restaurant
